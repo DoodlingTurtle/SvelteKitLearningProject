02d0b6fd993acfc3c6d31d1e28e97cce0a696228
DoodlingTurtle <52770682+DoodlingTurtle@users.noreply.github.com>
2022-12-07 06:25:36 +0100

    finished SwitchList functionality

--- src/lib/components/SwtichList.svelte
+++ src/lib/components/SwtichList.svelte
@@ -1,57 +1,138 @@
 <script>
-    export let source = { };
+    export let source = {};
     export let value = [];
+    export let height = 16;
+
+    let filter = "";
 
     $: selHighlight = { ...source };
-    $: for(let a in source) selHighlight[a] = false;
+    $: for (let a in source) selHighlight[a] = false;
 
-    $: noneselected = Object.keys(source).filter( (e) => value.indexOf(e) == -1 )
-    $: selected     = Object.keys(source).filter( (e) => value.indexOf(e) != -1 )
+    $: noneselected = Object.keys(source).filter((e) => value.indexOf(e) == -1);
+    $: selected = Object.keys(source).filter((e) => value.indexOf(e) != -1);
+
+    $: visible = Object.keys(source);
+
+    $: if (filter || source)
+        visible = Object.keys(source).filter((e) => {
+            const regExp = new RegExp(`${filter || ".*"}`, "gims");
+            const found = regExp.exec(source[e]);
+
+            if (!found) selHighlight[e] = false;
+
+            return found;
+        });
+
+    const l2r = () => {
+        noneselected.forEach((e) => {
+            if (selHighlight[e]) {
+                value.push(e);
+                selHighlight[e] = false;
+            }
+        });
+        value = value;
+    };
+
+    const r2l = () => {
+        selected.forEach((e) => {
+            if (selHighlight[e]) {
+                const index = value.indexOf(e);
+                if (index > -1) {
+                    value.splice(index, 1);
+                    selHighlight[e] = false;
+                }
+            }
+        });
+        value = value;
+    };
 </script>
 
+<div class="mb-2 mt-2">
+    <label for="filter">Filter:</label>
+    <input id="filter" type="text" bind:value={filter} />
+</div>
 
 <div class="container">
-    <div class="list">
+    <div class="list" style:height={height + "rem"}>
+        <label>available:</label>
         {#each noneselected as opt (opt)}
-            <button  class="mb-2 btnClean" class:sel={selHighlight[opt] === true} on:click={() => selHighlight[opt] = !selHighlight[opt]} > {@html source[opt]} </button>
+            {#if visible.indexOf(opt) > -1}
+                <button
+                    class="mt-2 btnClean"
+                    class:sel={selHighlight[opt] === true}
+                    on:click={() => (selHighlight[opt] = !selHighlight[opt])}
+                >
+                    {@html source[opt]}
+                </button>
+            {/if}
         {/each}
     </div>
 
-    <div>
-        <button>&rarr;</button>
-        <button>&larr;</button>
+    <div class="btnline">
+        <button class="btn" on:click={l2r}>&rarr;</button>
+        <button class="btn" on:click={r2l}>&larr;</button>
     </div>
 
-    <div class="list">
+    <div class="list" style:height={height + "rem"}>
+        <label>assigned:</label>
         {#each selected as opt (opt)}
-            <button class="mb-2 btnClean" class:sel={selHighlight[opt] === true} on:click={() => selHighlight[opt] = !selHighlight[opt]} > {@html source[opt]} </button>
+            {#if visible.indexOf(opt) > -1}
+                <button
+                    class="mt-2 btnClean"
+                    class:sel={selHighlight[opt] === true}
+                    on:click={() => (selHighlight[opt] = !selHighlight[opt])}
+                >
+                    {@html source[opt]}
+                </button>
+            {/if}
         {/each}
     </div>
 </div>
 
 <style lang="scss">
+    @import "../css/form.sass";
 
     .container {
         display: grid;
         grid-template-columns: 1fr 0fr 1fr;
-        grid-gap: .5rem;
+        grid-gap: 0.5rem;
+        align-items: center;
+
+        .btnline {
+            BUTTON {
+                margin: 0.2rem;
+                margin-left: 0;
+                margin-right: 0;
+
+                font-weight: bold;
+            }
+        }
 
         .list {
             display: inline-block;
+            background-color: white;
+            overflow-y: scroll;
+            overflow-x: hidden;
+            padding-block: 0.5rem;
+            border-radius: var(--border-rad);
 
             BUTTON {
-                background-color: var(--app-c-white);
+                background-color: lightgray;
                 padding: var(--border-rad);
                 border-radius: var(--border-rad);
-                width: calc(100% - var(--border-rad) - var(--border-rad));
+                width: calc(
+                    100% - var(--border-rad) - var(--border-rad) - 0.5rem
+                );
+                margin-inline: 0.25rem;
 
                 &.sel {
                     background-color: var(--accent-highlight);
                     color: var(--accent-text);
                 }
             }
-
         }
-
-    } 
+    }
+    LABEL {
+        font-weight: bold;
+    }
 </style>
--- src/routes/users/edit.html/+page.svelte
+++ src/routes/users/edit.html/+page.svelte
@@ -1,46 +1,93 @@
 <script>
-    import { fade } from 'svelte/transition';
-    import { page } from '$app/stores';
-    import { GET  } from "$lib/modules/API";
+    import { fade } from "svelte/transition";
+    import { page } from "$app/stores";
+    import { GET } from "$lib/modules/API";
 
     import PageTitle from "$lib/components/PageTitle.svelte";
     import Loader from "$lib/components/Loader.svelte";
-    import SwtichList from '../../../lib/components/SwtichList.svelte';
+    import SwtichList from "$lib/components/SwtichList.svelte";
+    import FoldContainer from "$lib/components/FoldContainer.svelte";
 
     let uid = parseInt($page.url.searchParams.get("uid") || "0");
     let userdata;
 
-    $: if(isNaN(uid)) uid = 0;
-    $: userDataPromise = GET(`/user/${uid}`, {expect: 'json'}).then( data => userdata = data );
+    $: if (isNaN(uid)) uid = 0;
+    $: userDataPromise = GET(`/user/${uid}`, { expect: "json" }).then(
+        (data) => (userdata = data)
+    );
 
     let availableModules = {
-        'opt1': "Option 1",
-        'opt2': "Option 2",
-        'opt3': "Option 3",
-        'opt4': "Option 4",
+        opt1: "Option 1",
+        opt2: "Option 2",
+        opt3: "Option 3",
+        opt4: "Option 4",
+        opt5: "Option 5",
+        opt14: "Option 14",
     };
 
-    let assignedModules = ['opt1', 'opt3'];
-
+    let assignedModules = ["opt1", "opt3"];
 </script>
 
 <div class="page-user-edit" in:fade>
     <a href="./list.html">&larr; back</a>
-    <PageTitle alternateContainer="#ApplicationTitleBar" scrollListener="#AppContainer">
+    <PageTitle
+        alternateContainer="#ApplicationTitleBar"
+        scrollListener="#AppContainer"
+    >
         Edit User {uid}
     </PageTitle>
 
     {#await userDataPromise}
-        <Loader /> 
-    {:then unused} 
-        // TODO Implement
+        <Loader />
+    {:then unused}
+        <FoldContainer>
+            <h2 slot="legend" class="btn">Modules:</h2>
 
-
-        <SwtichList source={availableModules} bind:value={assignedModules} />
-
-        
+            <fieldset slot="content">
+                <SwtichList
+                    height="16"
+                    source={availableModules}
+                    bind:value={assignedModules}
+                />
+            </fieldset>
+        </FoldContainer>
     {:catch err}
         {@const t1 = console.log(err)}
-        Something went wrong 
+        Something went wrong
     {/await}
 </div>
+
+<style lang="scss">
+    @import "colors.sass";
+    @import "media.sass";
+
+    :global([slot="legend"]) {
+        cursor: pointer;
+
+        margin-top: 0px;
+        margin-bottom: 0px;
+        font-size: 1em;
+
+        font-weight: bold;
+        display: block;
+        padding: 0.25rem;
+
+        box-shadow: 0 0 4px var(--shadow);
+        color: white;
+
+        border-radius: var(--border-rad);
+
+        @include gradient_bg;
+    }
+
+    :global(A.active:has([slot="legend"]) [slot="legend"]) {
+        border-radius: var(--border-rad) var(--border-rad) 0 0;
+    }
+
+    :global([slot="content"]) {
+        border: none;
+        background-color: var(--background);
+        box-shadow: 0 0 4px var(--shadow);
+        border-radius: 0 0 var(--border-rad) var(--border-rad);
+    }
+</style>
