1e0564a97121fde4428e56301ef25ac21d0af684
DoodlingTurtle <52770682+DoodlingTurtle@users.noreply.github.com>
2022-12-09 06:55:18 +0100

    Fixed blocking issues when opening and closing IndexedDB

--- src/lib/modules/database.js
+++ src/lib/modules/database.js
@@ -1,10 +1,13 @@
 import { browser } from "$app/environment";
-import {openDB, deleteDB, unwrap } from 'idb'
-import { get } from "svelte/store";
+import {openDB, deleteDB } from 'idb'
+
+import DebugModule from '$lib/modules/Debug'
 
 const DBNAME = "app_storage";
 
 let DB = null;
+
+const debug = DebugModule.prefix("database");
 
 /** 
  * 'opts' parameter available to DatabaseStore 
@@ -38,6 +41,8 @@ export class DatabaseStore {
         this._key = `${name}`;
         this._storeOpts = opts;
         this._indexes = indexes;
+        this._db = null;
+        this._debug = debug.prefix(`.DatabseStore(${name})`)
     }
 
     /**
@@ -45,12 +50,36 @@ export class DatabaseStore {
      * @param {IDBTransaction} tx 
      */
     install = async (db, tx) => {
+        this._debug.log("install");
         let store = await db.createObjectStore(this._key, this._storeOpts);
         for(let a in this._indexes) {
             await store.createIndex(a, this._indexes[a].keyPath, this._indexes[a].options)
         }
     }
 
+
+    /** 
+     * @callback DatabaseHandlerTransactionCallback
+     * @param {IDBObjectStore} store
+     * @param {IDBTransaction} tx
+     * @return {Promise.<any>}
+     */
+
+    /**
+     * starts a transactions and provieds it to the handler 
+     * @param {DatabaseHandlerTransactionCallback} handler 
+     * @param {IDBTransactionMode} mode 
+     */
+    tx = async (handler, mode='readonly') => {
+        let debug = this._debug.prefix(`.tx(callback, ${mode})`);
+        /** IDBDatabase */
+        if(this._db) {
+            debug.log("transaction start in db", this._db);
+            const tx = await this._db.transaction(this._key, mode );
+            await handler(tx.store, tx, debug);
+        }
+        else throw new Error("no database");
+    }
     name() { return this._key };
 }
 
@@ -65,39 +94,77 @@ if(browser) {
 }
 
 async function getDB() {
+    let _debug = debug.prefix(".getDB()");
+    _debug.log("is browser", browser);
     if(!browser) return;
+
     if(!DB) {
         try {
-            DB = openDB(DBNAME, version, {
-                upgrade: (db, oldV, newV, tx) => {
-                    let store = stores.shift();
+            let retry = true;
+            while(retry) {
+                retry = false;
 
-                    while(store) {
-                        store.install(db, tx);
-                        store = stores.shift()
+                DB = await openDB(DBNAME, version, {
+                    upgrade: (db, oldV, newV, tx) => {
+                        const __debug = _debug.prefix(".onUpdate");
+                        let store = stores.shift();
+
+                        while(store) {
+                            if(!db.objectStoreNames.contains(store.name()))
+                                store.install(db, tx);
+                            else __debug.warn(` store '${store.name()}' exists`);
+
+                            store = stores.shift()
+                        }
+
+                        localStorage.setItem(`${DBNAME}_db_version`, version);
+                        __debug.log("finished", db, oldV, newV, tx);
+                    },
+                
+                    blocked: (curV, blockedV, ev) => {
+                        _debug.prefix(".onBlocked")
+                              .error(`version ${blockedV} is blocked by version ${curV}`, ev, ev.currentTarget);
+                    },
+
+                    blocking: async (curV, blockedV, ev) => {
+                        const __debug = _debug.prefix(".onBlocking");
+                        __debug.warn(`version ${curV} is blocking ${blockedV}`, ev);
+                        await ev.currentTarget.close()
+                        __debug.log(`version ${curV} closed => retry open`)
+                        retry = true; 
+                    },
+
+                    terminated: (ev) => {
+                        _debug.prefix(".onTerminated")
+                              .warn("connection has been terminated", ev)
                     }
-
-                    localStorage.setItem(`${DBNAME}_db_version`, version);
-                },
-            
-                blocked: (curV, blockedV, ev) => {
-                    console.error(`Database version ${blockedV} is blocked by version ${curV}`, ev);
-                },
-
-                blocking: (curV, blockedV, ev) => {
-                    console.error(`Database version ${curV} is blocking ${blockedV}`, ev);
-                }
-            });
+                });
+            }
+            _debug.log("got db", DB); 
+            return DB;
         }
         catch(err) {
-            console.error("[database.getDB] error", err)
-            DB = Promise.resolve(null);
+            _debug.error("error", err)
         }
+    }
+    else {
+        _debug.log("database already open", DB); 
         return DB;
     }
-    if(!DB) throw "could not get DB";
 
-    return DB;
+    throw new Error("[database.getDB] could not open Database");
+}
+
+
+async function closeDB() {
+    const _debug = debug.prefix(".closeDB()")
+    while(DB) {
+        try { 
+            await (await getDB()).close(); 
+            DB = null;
+        }
+        catch(err) { _debug.error(err); }
+    }
 }
 
 
@@ -108,47 +175,48 @@ export const registerStore = async (store) => {
     if(!browser) return;
     if(!(store instanceof DatabaseStore)) throw new Error("not a DatabaseStore instance");
 
+    const _debug = debug.prefix(`.registerStore(${store.name()})`);
+
+    _debug.log('start');
+
     let db = await getDB();
-    if(db && db.objectStoreNames.contains(store.name()) ) return; //throw new Error('store exists');
+    if(db && db.objectStoreNames.contains(store.name()) ) {
+        _debug.log('store exists');
+        store._db = db;
+        return; //throw new Error('store exists');
+    }
     
     stores.push(store);
     allStores.push(store);
-
     version++;
 
-    await (() => new Promise(async (reject, resolve) => {
-        try { await db.close(); DB = null; }
-        catch( err ) { console.error("failed to close db", err) }
-
-        try { await getDB(); }
-        catch( err ) { console.error("failed to reopen db", err) }
-    }))()
-
+    await closeDB();
+    db = await getDB();
+    store._db = db;
 };
 
 export const clear = async () => {
+    const _debug = debug.prefix(".clear()")
     if(!browser) return;
-    console.log(DB);
-    if(DB) {
-        try { await (await getDB()).close(); }
-        catch(err) { console.warn("[database.clear] warning ", err); }
-    }
-    DB = null;
 
-    let success = false;
+    await closeDB();
 
-    while(!success) {
-        success = true;
+    let retry = true;
+
+    while(retry) {
+        retry = false;
+
         await deleteDB(DBNAME, {
-            blocked: (curV, ev) => {
-                console.error(`Database version ${curV} is blocked from deletion`, ev);
-                success = false;
+            blocked: async (curV, ev) => {
+                _debug.error(`version ${curV} is blocked from deletion => retry`, ev);
+                await ev.currentTarget.close();
+                retry = true;
             }
         })
     }
+
     localStorage.removeItem(`${DBNAME}_db_version`);
+    version = 1;
 }
-
-
 
 export default { DatabaseStore, registerStore, clear}
