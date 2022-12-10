91742ff0813bd6ea0be20b68055b1ae9f1c8cd8b
DoodlingTurtle <52770682+DoodlingTurtle@users.noreply.github.com>
2022-12-10 06:32:28 +0100

    API should not handle SSR requests better reactiong with a 503 response when they happen (SSR is not allowed fo this project because Static-BuildTarget)

--- src/lib/modules/API.js
+++ src/lib/modules/API.js
@@ -1,5 +1,9 @@
 // @ts-nocheck
 import {api_token, api_url} from '$lib/modules/Stores'
+import { browser } from '$app/environment';
+import DebugModule from '../modules/Debug'
+
+const debugbase = DebugModule.prefix("API")
 
 const statusEventHandlers = { }
 export function addResponseListener(httpStatus, handler) {
@@ -21,7 +25,10 @@ let Authorization = "";
 api_token.subscribe(obj => Authorization = `Bearer ${obj}` );
 api_url  .subscribe(obj => apiurl = obj  );
 
-function call(method, url, opts) {
+async function call(method, url, opts, debugbase) {
+
+	const debug = debugbase.prefix("("+url+")")
+
 	const resType = opts.expect || "";
 	const ignore = {};
 
@@ -46,70 +53,77 @@ function call(method, url, opts) {
 
 	options.headers.Authorization = Authorization
 
-	return new Promise(async (resolve, reject) => {
+	const processResponse = async (data) => {
+		const _debug = debug.prefix(".processResponse()")
 
-		if(typeof(apiurl) != 'string' || apiurl.trim() == "") {
-			let settings = await (await fetch("/settings.json")).json();
-			apiurl = settings.defaultAPIURL;
-			api_url.set(settings.defaultAPIURL);
+		switch (resType) {
+			case 'json'		  : data.data = await data.json();        break;
+			case 'text'		  : data.data = await data.text();        break;
+			case 'formData'	  : data.data = await data.formData();    break;
+			case 'blob'		  : data.data = await data.blob();        break;
+			case 'arrayBuffer': data.data = await data.arrayBuffer(); break;
 		}
 
-		/** @type {Response|null} */
-		let oRes = null;
-		fetch(`${apiurl}${url}`, options)
-			.then(data => {
+		switch(data.status) {
+			case 200:	/* OK */
+			case 201:	/* Created */
+			case 202:	/* Accepted */
+			case 204:	/* No Content */
+			case 205:	/* Reset Content */
+			case 206:	/* Partial Content */
+				return data;
 
-				switch(data.status) {
-					case 200:	/* OK */
-					case 201:	/* Created */
-					case 202:	/* Accepted */
-					case 204:	/* No Content */
-					case 205:	/* Reset Content */
-					case 206:	/* Partial Content */
-						oRes = data;
-						switch (resType) {
-							case 'json': return data.json();
-							case 'text': return data.text();
-							case 'formData': return data.formData();
-							case 'blob': return data.blob();
-							case 'arrayBuffer': return data.arrayBuffer();
-							default: return Promise.resolve(undefined)
-						}
-						break;
+			default:
+				if(data.status != 503)
+					_debug.warn("unexpected response", data);
 
-					default:
-						console.warn("unexpected response", data);
-						if(!ignore[data.status] && statusEventHandlers[data.status]) {
-							statusEventHandlers[data.status].forEach( (h) => {
-								h(data);
-							} )
-						}
-
-						case 'text': data.text().then(txt => {
-							data.data = txt;
-							reject(data)
-						});
-						return null;
-				}
-
-			})
-			.then(data => {
-				if(oRes) {
-					oRes.data = data
-					resolve(oRes);
-				}
-			})
-			.catch( (...args) => {
-				console.error("error during fetch", args);
-				if(oRes && oRes.status && !ignore[oRes.status] && statusEventHandlers[oRes.status]) {
-					statusEventHandlers[oRes.status].forEach( (h) => {
-						h(oRes);
+				if(!ignore[data.status] && statusEventHandlers[data.status]) {
+					statusEventHandlers[data.status].forEach( (h) => {
+						h(data);
 					} )
 				}
 
-				reject.apply(null, args)
-			} )
-	});
+				throw data;	
+		}
+	}
+
+	if(!browser) {
+		debug.log("is Server side => respond with 503 Service Unavailabe");
+
+		let resdata = null;
+		switch (resType) {
+			case 'json'		  : resdata = new ReadableStream("{}");   break;
+			case 'formData'	  : resdata = new FormData();   break;
+			case 'blob'		  : resdata = new Blob();   break;
+			case 'arrayBuffer': resdata = new ArrayBuffer();   break;
+			default			  : resdata = new ReadableStream("");   break;
+		}
+
+		const response = new Response(resdata, {
+			status: 503, statusText: "Service Unavailable"
+		});
+
+		return await processResponse(response)
+	}
+	else {
+		return (async () => {
+
+			if(typeof(apiurl) != 'string' || apiurl.trim() == "") {
+				debug.warn("missing apiurl => fetching /settings.json first");
+				let settings = await (await fetch("/settings.json")).json();
+
+				apiurl = settings.defaultAPIURL;
+				debug.log("got settings => update apiurl to ", apiurl);
+				api_url.set(settings.defaultAPIURL);
+			}
+
+			debug.log("run fetch")
+			const res = await processResponse(await fetch(`${apiurl}${url}`, options));
+			debug.log("got", res)
+			return res;
+
+		})()
+	}
 }
 
 function contentTypePreProcess(opts={}, body=null) {
@@ -153,16 +167,16 @@ function contentTypePreProcess(opts={}, body=null) {
 	}
 }
 
-export function GET(url, opts={}) 		 { return call("get", url, opts); }
-export function DELETE(url, opts={}) 	 { return call("delete", url, opts); }
+export function GET(url, opts={}) 		 { return call("get", url, opts, debugbase.prefix(".GET")); }
+export function DELETE(url, opts={}) 	 { return call("delete", url, opts, debugbase.prefix(".DELETE")); }
 export function POST(url, body, opts={}) { 
-	return call("post", url, contentTypePreProcess(opts, body) ) 
+	return call("post", url, contentTypePreProcess(opts, body), debugbase.prefix(".POST") ) 
 }
 export function PUT(url, body, opts={}) { 
-	return call("put", url, contentTypePreProcess(opts, body) ) 
+	return call("put", url, contentTypePreProcess(opts, body), debugbase.prefix(".PUT") ) 
 }
 export function PATCH(url, body, opts={}) { 
-	return call("PATCH", url, contentTypePreProcess(opts, body) )
+	return call("PATCH", url, contentTypePreProcess(opts, body), debug.debugbase.prefix(".PATCH") )
 }
 
 export default { GET, POST, PUT, PATCH, DELETE, addResponseListener, removeResponseListener }
